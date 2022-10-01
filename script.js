35578f12da37d04f6d8d8f63fd88e05c7017afbe
zoelin212 <zoelin212@gmail.com>
2022-10-01 11:34:34 -0600

    updat js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-fetch('https://services.swpc.noaa.gov/products/summary/solar-wind-speed.json')
+fetch('https://services.swpc.noaa.gov/products/summary/10cm-flux.json')
   .then((response) => response.json())
   .then((data) => {
-    console.log(data.TimeStamp)
-    let speed  = data.WindSpeed;
+    console.log(data.Flux)
+    let speed  = data.Flux;
     msg.innerHTML="The speed of solar wind is "+speed;
 });
 

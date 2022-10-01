5caa4b3215790d628266aed7487adb37a0e2ae51
Tîng-huē Tân <71164837+hejkeikei@users.noreply.github.com>
2022-10-01 12:27:35 -0600

    update
    -world map svg
    - fetch function build

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-
-fetch('https://services.swpc.noaa.gov/products/noaa-estimated-planetary-k-index-1-minute.json')
-  .then((response) => response.json())
-  .then((data) => {
-    let now = data[data.length-1]
-    console.log(now[1]);
-    
-});
+// Get the real time kp value
+function getKpNow(){
+  fetch('https://services.swpc.noaa.gov/products/noaa-estimated-planetary-k-index-1-minute.json')
+    .then((response) => response.json())
+    .then((data) => {
+      let now = data[data.length-1]
+      console.log(now[1]);
+  });
+}
+showNow.addEventListener("click",getKpNow);
 

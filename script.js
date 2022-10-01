e85aa1f2db9e6b98e5a8875fda830ca0ebf60ad8
Tîng-huē Tân <71164837+hejkeikei@users.noreply.github.com>
2022-10-01 10:30:10 -0600

    update

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const req = new XMLHttpRequest();
-req.open("GET", "https://services.swpc.noaa.gov/products/summary/solar-wind-speed.json", true);
-req.send();
-req.onload = function () {
-  const json = JSON.parse(req.responseText);
-  console.log(json)
-};
+
+fetch('https://services.swpc.noaa.gov/products/summary/solar-wind-speed.json')
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data.TimeStamp)
+    let speed  = data.WindSpeed;
+    msg.innerHTML="The speed of solar wind is "+speed;
+});
+

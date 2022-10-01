e2a0983453f8dcc5ca4e673c52b42ad83a430785
Collin Park <oliveguy85@gmail.com>
2022-10-01 13:04:36 -0600

    basic templete creation

--- server.js
+++ server.js
@@ -2,26 +2,14 @@ var http = require('http');
 var fs = require('fs');
 var app = http.createServer(function(request,response){
     var url = request.url;
-    let now = new Date();
     if(request.url == '/'){
-      let templete = 
-    `
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-          <meta charset="UTF-8" />
-          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>Infinity and Beyond</title>
-      </head>
-    `;
+      url = '/index.html';
     }
     if(request.url == '/favicon.ico'){
       return response.writeHead(404);
     }
     response.writeHead(200);
-    let templete =
-    `
+    let tem = `
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -31,22 +19,11 @@ var app = http.createServer(function(request,response){
         <title>Infinity and Beyond</title>
     </head>
     <body>
-        <h1>LIHO</h1>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
-        <p id="msg"></p>
-        <script src="script.js"></script>
-        <p>Now time is${now}</p>
-  </body>
-</html>
-    `;
-
-    response.end(templete);
+    
+    </body>
+    </html>
+    `
+    response.end(tem);
  
 });
 app.listen(3000);

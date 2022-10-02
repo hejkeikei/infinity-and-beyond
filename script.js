16454518d373de5d81ca9b95a66a78ef6f5d33b9
Tîng-huē Tân <71164837+hejkeikei@users.noreply.github.com>
2022-10-02 12:07:15 -0600

    map working / aurora working

--- script.js
+++ script.js
@@ -27,87 +27,57 @@ function includeHTML() {
 }
 includeHTML();
 // Get the real time kp value
+
+// setTimeout(canv, 4000);
 function getKpNow() {
   fetch('https://services.swpc.noaa.gov/products/noaa-estimated-planetary-k-index-1-minute.json')
     .then((response) => response.json())
     .then((data) => {
       let now = data[data.length - 1]
-      // console.log(now[1]);
+      console.log(now[1]);
       let nowVal = now[1] * 10;
-      glow.style.background =
-        "radial-gradient(closest-side,rgba(221, 250, 114, " + now[1] / 10 + ") 0%,rgba(128, 250, 57, " + now[1] / 10 + ")" +
-        nowVal +
-        "%,rgba(0, 212, 255, 0) 100%)";
+      var canv = document.querySelector("canvas");
+      canv.style.opacity = now[1] / 10;
     });
 }
 showNow.addEventListener("click", getKpNow);
-
-// n2.addEventListener("input", () => {
-//   console.log("nitrogen val is: " + n2.value);
-//   n2Val.innerHTML = n2.value;
-//   generateAurora();
-// });
-// ox.addEventListener("input", () => {
-//   console.log("oxygen val is: " + ox.value);
-//   oxVal.innerHTML = ox.value;
-//   generateAurora();
-// });
-// wind.addEventListener("input", () => {
-//   console.log("wind val is: " + wind.value);
-//   windVal.innerHTML = wind.value;
-//   generateAurora();
-// });
-// date.addEventListener("input", () => {
-//   console.log("user choose date: " + date.value);
-// });
 
 const inputs = document.querySelector("form");
 const glow = document.querySelector("main");
 
 glow.style.background = "radial-gradient(closest-side,rgba(221, 250, 114, 0.1) 0%,rgba(128, 250, 57, 0.1) 30%,rgba(0, 212, 255, 0) 100%)";
-// aurora calculation
-// function generateAurora() {
-//   console.log("change");
-//   let green = wind.value;
-//   let red = ox.value * 5;
-//   let pink = n2.value;
-//   if (red != 0) {
-//     glow.style.background =
-//       "radial-gradient(closest-side,rgba(221, 250, 114, 1) 0%,rgba(128, 250, 57, 1)" +
-//       green +
-//       "%,rgba(232, 52, 49,0.5) " + red + "%,rgba(0, 212, 255, 0) 100%)";
-//   } else if (pink != 0) {
-//     let rgbval = (161, 72, 66);
-//     glow.style.background =
-//       "radial-gradient(closest-side,rgba(221, 250, 114, 1) 0%,rgba(128, 250, 57, 1)" +
-//       green +
-//       "%,rgba(0, 212, 255, 0) 100%)";
-//   } else if (green < 2) {
-//     glow.style.background =
-//       "radial-gradient(closest-side,rgba(221, 250, 114, 0.1) 0%,rgba(128, 250, 57, 0.1)" +
-//       green +
-//       "%,rgba(0, 212, 255, 0) 100%)";
-//   } else {
-//     glow.style.background =
-//       "radial-gradient(closest-side,rgba(221, 250, 114, 0.8) 0%,rgba(128, 250, 57, 0.8)" +
-//       green +
-//       "%,rgba(0, 212, 255, 0) 100%)";
-//   }
-// };
+
 
 // choose location
 function pathGen() {
-  const worldMap = document.querySelectorAll("path");
-  var place = document.createElement("p");
+  var canv = document.querySelector("canvas");
+  const mapA = document.querySelector("#a");
+  const mapB = document.querySelector("#b");
+  const mapC = document.querySelector("#c");
+  const mapD = document.querySelector("#d");
+  const mapE = document.querySelector("#e");
   // console.log(worldMap);
-  worldMap.forEach((e) => {
-    let name = e.getAttribute("name");
-    // console.log(name);
-    e.addEventListener("click", () => {
-      console.log("click");
-    })
-  })
+  mapA.addEventListener("click", (item) => {
+    canv.style.opacity = "1";
+    // console.log(mapA);
+  });
+  mapB.addEventListener("click", (item) => {
+    canv.style.opacity = "0.8";
+    // console.log(mapB);
+  });
+  mapC.addEventListener("click", (item) => {
+    canv.style.opacity = "0.4";
+    // console.log(mapC);
+  });
+  mapD.addEventListener("click", (item) => {
+    canv.style.opacity = "0.6";
+    // console.log(mapD);
+  });
+  mapE.addEventListener("click", (item) => {
+    canv.style.opacity = "0";
+    // console.log(mapE);
+  });
 }
 
-setTimeout(pathGen, 5000);
+setTimeout(pathGen, 3000);
 

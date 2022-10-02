f1f7ecedeb0023f51c8ae5c6b2cbc67f66f39ac8
Collin Park <oliveguy85@gmail.com>
2022-10-02 17:57:26 -0600

    date selectors and changeable opacity

---
+++ index.php
@@ -0,0 +1,108 @@
+<?php
+// DB_Connection
+$server = "localhost";
+$username = "vslbau15_spaceuser_master";
+$password = "asdf4321ASDF";
+$database = "vslbau15_spaceApp";
+$connection = mysqli_connect($server, $username, $password, $database);
+if(!$connection){
+    die(mysql_connect_error());
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Infinity and Beyond</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="styles.css" />
+    <link rel="stylesheet" href="aurora.css" />
+</head>
+
+<body>
+    <main>
+
+        <div id="mountain">
+            <!-- Location -->
+
+            <div id="mapBox" w3-include-html="map.html">
+                <!-- <div w3-include-html="map.html"></div> -->
+            </div>
+
+
+            <div id="infoBox">
+                <header>
+                    <h1>Enjoy the aurora from the solar wind anytime and anywhere!</h1>
+                </header>
+
+                <!-- shows real time -->
+                <div>
+                    <button type="button" id="showNow">Show Me Now</button>
+                </div>
+
+                <p> OR </p>
+                <form action="#" method="GET">
+<ul id="dateSelect">
+<!-- 7 Days history including today -->
+<?php
+//Getting Solar wind data from DB
+$query = "SELECT date, speed FROM electron_fluence_forecast ORDER BY te DESC LIMIT 7";
+$sql = mysqli_query($connection, $query);
+// showing <a>tags for 7 day
+while($data = mysqli_fetch_array($sql)){
+    $newDate = substr($data['date'],0,strlen($string)-5);
+    echo '<a href="index_php.php" id="btnspeed'.$data['speed'].'" class="datechoice">'.$newDate.'</a>';
+        // Opacity calculator
+        $figure = 0;
+        if($data['speed']<500){
+            $figure = 0;
+        } elseif($data['speed']>500){
+            $figure = $data['speed']/1000;
+        } else{
+            $figure = 0;
+        }
+    echo "
+    <script>
+        ".
+        "btnspeed".$data['speed'].".addEventListener('click',(e)=>{
+            e.preventDefault();
+            var canv = document.querySelector('canvas');
+            canv.style.opacity = ".$figure.";
+        })"
+        ."
+    </script>
+    ";
+}
+?>
+</ul>
+
+                    <!-- <label for="date"></label> -->
+
+                    <div id="rangeBox">
+                        <div>
+                            <label for="wind">Solor wind</label>
+                            <p class="inlineBlock">
+                                <input type="range" id="wind" name="wind" min="0" max="100" value="0" /><span id="windVal"></span>%</p>
+                        </div>
+
+                        <div>
+                            <label for="n2">N<sub>2</sub><sup>&#43;</sup></label>
+                            <p class="inlineBlock">
+                                <input type="range" id="n2" name="n2" min="0" max="78" value="0" /><span id="n2Val"></span>%</p>
+                        </div>
+
+                        <div>
+                            <label for="ox">O</label>
+                            <p class="inlineBlock">
+                                <input type="range" id="ox" name="ox" min="0" max="21" value="0" /><span id="oxVal"></span>%</p>
+                        </div>
+                    </div>
+                </form>
+            </div>
+    </main>
+    <script src="script.js"></script>
+    <script src="aurora.js"></script>
+</body>
+
+</html>
